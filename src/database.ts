3136e82d5c9d88144a834ac5988ae923e5b861e9
Zoltanbi <zoltanbiro001@gmail.com>
2022-01-07 18:49:11 -0800

    create database manager

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { Connection, Repository, createConnection } from "typeorm";
+import { User } from "./entity/User";
+
+export class Database {
+
+    public static connection: Connection;
+    public static userRepository: Repository<User>;
+
+    public static async initialize() {
+        this.connection = await createConnection();
+        this.userRepository = this.connection.getRepository(User);
+    }
+}
