bcd66454b6227b88851d5278ab6dd6847c6c07d6
Zoltanbi <zoltanbiro001@gmail.com>
2022-01-11 15:44:58 -0800

    create error handling middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express"
+import { HttpError } from "../error/httpError"
+
+export const errorMiddleware = (
+    err,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (err instanceof HttpError) {
+        const httpError: HttpError = <HttpError>err;
+
+        res.status(httpError.statusCode).json({
+            statusCode: httpError.statusCode,
+            message: httpError.message,
+        });
+
+        return;
+    }
+
+    res.status(500).json({
+        statusCode: 500,
+        message: err.message,
+    });
+};
